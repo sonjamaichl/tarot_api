3bbddec5c4088a82e58298218e7fcb46f237c7f9
sonjamaichl <sonja.maichl@gmail.com>
2023-05-11 13:58:58 +0200

    new routes added + user info about available endpoints on landing page updated

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,7 @@
 const allCardsRoutes = require('./allcards');
 const nameRoutes = require('./name');
+const randomRoutes = require('./random');
+const meaningRoutes = require('./meaning');
 
 const appRouter = (app, fs) => {
 app.get('/', (req, res) => {
@@ -8,12 +10,18 @@ app.get('/', (req, res) => {
     res.status(200).json({
         welcome: 'Welcome to my new tarot API.',
         info: 'The API contains info about all 78 tarot cards. You can use the following endpoints to fetch data:',
-        endpoints: [{'/allcards' : 'get the whole dataset with all 78 cards'}, {'/name' : 'find all cards whose name contain whatever you type as "name"'}]
+        endpoints: [
+            {'/allcards' : 'get the whole dataset with all 78 cards'}, 
+            {'name/x' : 'find all cards with name that contains x'}, 
+            {'meaning/y' : 'find all cards that contain y in their keywords or meanings properties'},
+            {'/cards/random' : 'get one random card'}]
     })
 });
 
 allCardsRoutes(app, fs);
 nameRoutes(app, fs);
+randomRoutes(app, fs);
+meaningRoutes(app, fs);
 };
 
 module.exports = appRouter;
