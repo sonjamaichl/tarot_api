4b6b9e7052c86f50ee00408a3e60d1f68cf4cfb5
sonjamaichl <sonja.maichl@gmail.com>
2023-05-11 13:56:05 +0200

    path changed for endpoint to search by name

--- routes/name.js
+++ routes/name.js
@@ -3,16 +3,17 @@ const nameRoutes = (app, fs) => {
     const dataPath = './data/allcards.json';
 
     //READ
-    app.get('/:name', (req, res) => {
+    app.get('/name/:name', (req, res) => {
         fs.readFile(dataPath, 'utf8', (err, data) => {
             if (err) {
                 throw err;
             }
-
+        //SEARCH FOr MATCHES AND STORE THEM IN NEW ARRAY
         const name = req.params.name;
         const result = JSON.parse(data);
         const cards = result.cards;
         const matches = cards.filter(card => card.name.toLowerCase().includes(name));
+        //SEND RESPONSE BACK TO CLIENT
         res.header("Access-Control-Allow-Origin", "*");     //* is allowing all clients to fetch
         res.status(200).json(matches);
         });
